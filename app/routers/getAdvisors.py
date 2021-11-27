a487196bb35bbfa6b29a9e7da7d3ac14bf4e4445
SanketWagh <firearmz0147@gmail.com>
2021-11-27 23:56:14 +0530

    html response get ll advisors complete

--- app/routers/getAdvisors.py
+++ app/routers/getAdvisors.py
@@ -1,8 +1,8 @@
-
 from typing import List  # list is used for response to send back list 
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
-from .. import models, schemas, oauth
+from fastapi.responses import HTMLResponse
+from .. import models, schemas
 from ..database import get_db
 
 
@@ -15,17 +15,41 @@ router = APIRouter(
     tags=["Users"]   
 )
 
-@router.get("/{id}/advisor", response_model=List[schemas.AdvisorsOut])  # return a list of response i based on schema model
+@router.get("/{id}/advisor", response_class=HTMLResponse)  # return a list of response i based on schema model
 def get_posts(db:Session = Depends(get_db)): 
 
     posts = db.query(models.Advisor)  
     print(posts)
 
-    ret_list = []
+    advisor_list = []
     for each in posts:
         temp = {}
         temp["name"] = each.name
         temp["id"] = each.id
         temp["image_url"] = each.image_url
-        ret_list.append(temp)
-    return ret_list
+        advisor_list.append(temp)
+    # print("----------------------------------------------------------------------------------------------------")
+    # print(advisor_list)
+    # print("----------------------------------------------------------------------------------------------------")
+    return html_string(advisor_list)
+
+
+def html_string(advisors: list):
+    all_advisors =""
+    for each_advisor in advisors:
+        all_advisors +=f" <h{2}>Advisor ID : {each_advisor['id']}</h{2}> "
+        all_advisors +=f" <h{2}>Advisor Name : {each_advisor['name']}</h{2}> "
+        all_advisors +=f'''<img src={each_advisor['image_url']} width="400" height="500"">'''
+
+    base_html = f"""
+                    <html>
+                        <head>
+                            <title>Super Hero Advisors</title>
+                        </head>
+                        <body>
+                            <h1> All Advisors List </h1>
+                            {all_advisors}
+                        </body>
+                    </html>
+                    """
+    return base_html
