81c5aa70972f019926b425ae0ba442d65b432bd5
SanketWagh <firearmz0147@gmail.com>
2021-11-29 10:17:44 +0530

    moved booking up

--- app/routers/getAdvisors.py
+++ app/routers/getAdvisors.py
@@ -61,46 +61,6 @@ def book(book :schemas.Book, db:Session = Depends(get_db), id : int = Depends(oa
 
 
 
-# GEt all advisors 
-@router.get("/{id}/advisors",status_code=200, response_class=HTMLResponse) 
-def get_advisors(id: int = Depends(oauth.get_current_user), db:Session = Depends(get_db)): 
-
-    advisors = db.query(models.Advisor)  
-    current_user_id = id.id
-
-
-    advisor_list = []
-    for each in advisors:
-        temp = {}
-        temp["name"] = each.name
-        temp["id"] = each.id
-        temp["image_url"] = each.image_url
-        advisor_list.append(temp)
-    return html_string(advisor_list, current_user_id)
-
-
-def html_string(advisors: list, current_user_id = int):
-    all_advisors =""
-    for each_advisor in advisors:
-        all_advisors +=f" <h{2}>Advisor ID : {each_advisor['id']}</h{2}> "
-        all_advisors +=f" <h{2}>Advisor Name : {each_advisor['name']}</h{2}> "
-        all_advisors +=f'''<img src={each_advisor['image_url']} width="400" height="500"">'''
-
-    base_html = f"""
-                    <html>
-                        <head>
-                            <title>Super Hero Advisors</title>
-                        </head>
-                        <body>
-                            <h1> All Advisors List </h1>
-                            <h1>Current User ID: {current_user_id}</h1>
-                            {all_advisors}
-                        </body>
-                    </html>
-                    """
-    return base_html
-
-
 # Get only booked advisors by specific user
 @router.get("/{id}/advisor/booking",status_code=200, response_class=HTMLResponse)  
 def book(db:Session = Depends(get_db), id : int = Depends(oauth.get_current_user_id)):
@@ -132,6 +92,46 @@ def html_string_2(user_booked_advisor_list, current_user_id):
                         <body>
                             <h1> All Advisors Booked by  </h1>
                             <h1> User ID: {current_user_id}</h1>
+                            {all_advisors}
+                        </body>
+                    </html>
+                    """
+    return base_html
+
+
+# GEt all advisors 
+@router.get("/{id}/advisor",status_code=200, response_class=HTMLResponse) 
+def get_advisors(id: int = Depends(oauth.get_current_user), db:Session = Depends(get_db)): 
+
+    advisors = db.query(models.Advisor)  
+    current_user_id = id.id
+
+
+    advisor_list = []
+    for each in advisors:
+        temp = {}
+        temp["name"] = each.name
+        temp["id"] = each.id
+        temp["image_url"] = each.image_url
+        advisor_list.append(temp)
+    return html_string(advisor_list, current_user_id)
+
+
+def html_string(advisors: list, current_user_id = int):
+    all_advisors =""
+    for each_advisor in advisors:
+        all_advisors +=f" <h{2}>Advisor ID : {each_advisor['id']}</h{2}> "
+        all_advisors +=f" <h{2}>Advisor Name : {each_advisor['name']}</h{2}> "
+        all_advisors +=f'''<img src={each_advisor['image_url']} width="400" height="500"">'''
+
+    base_html = f"""
+                    <html>
+                        <head>
+                            <title>Super Hero Advisors</title>
+                        </head>
+                        <body>
+                            <h1> All Advisors List </h1>
+                            <h1>Current User ID: {current_user_id}</h1>
                             {all_advisors}
                         </body>
                     </html>
